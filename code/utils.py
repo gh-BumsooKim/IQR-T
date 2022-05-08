2922d7227923648cefaae42cd088fc6c71529714
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2022-05-08 19:14:53 +0900

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,25 @@
 import numpy as np
 
+import argparse
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    
+    # Basic Options
+    parser.add_argument("--cont_prefix", type=str, default=None,
+                        help="path with prefix of content images")
+    
+    parser.add_argument("--calibration_config", type=str, default=None,
+                        help="path of the file containing the camera-projector\
+                            calibration parameters")
+
+    parser.add_argument("--track_mode", type=int, default=0,
+                        help="mode about region tracking")                        
+                            
+    # Advanced Options
+    
+    return parser
+
 def point_sort(pts):
     index = np.argsort(pts[:, 0])
 
